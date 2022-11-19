793703da0893eb220b4c1326bad4acb3fbe74b02
Samir EL Hajjoui <r0897980@student.thomasmore.be>
2022-11-19 15:28:17 +0100

    update2

--- app/api.py
+++ app/api.py
@@ -7,10 +7,11 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost/",
-    "http://localhost:8080/",
-    "https://localhost.tiangolo.com/",
-    "http://127.0.0.1:5500/"
+    # "http://localhost/",
+    # "http://localhost:8080/",
+    # "https://localhost.tiangolo.com/",
+    # "http://127.0.0.1:5500/"
+    "*"
 ]
 
 app.add_middleware(
